08882c5e39209594396a20fb5bb3f9e66a5f8562
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-07-20 13:08:21 +0530

    feat(fonts): switch to Roboto and Roboto Mono for improved typography

--- v0/v0/client/app/layout.tsx
+++ v0/v0/client/app/layout.tsx
@@ -1,24 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Instrument_Sans } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import QueryProvider from "@/components/providers/query-provider";
 import { Toaster } from "@/components/ui/sonner";
 
-const instrumentSansHeading = Instrument_Sans({
+const robotoSans = Roboto({
+  weight: ["400", "500", "700"],
   subsets: ["latin"],
-  variable: "--font-heading",
+  variable: "--font-roboto-sans",
 });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const robotoMono = Roboto_Mono({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-roboto-mono",
 });
 
 export const metadata: Metadata = {
@@ -37,9 +33,8 @@ export default function RootLayout({
       className={cn(
         "h-full",
         "antialiased",
-        geistSans.variable,
-        geistMono.variable,
-        instrumentSansHeading.variable,
+        robotoSans.variable,
+        robotoMono.variable,
       )}
       suppressHydrationWarning
     >
